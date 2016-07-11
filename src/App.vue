653b67cd73cda28873cd235eb9e80f1070cb20b6
Nati Namvong <nati@flyingcomma.com>
2016-07-12 01:35:44 +0700

    view data templete

---
+++ src/App.vue
@@ -0,0 +1,243 @@
+<template>
+
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile has-text-right subtitle">12:30</div>
+        <div class="column is-3 is-hidden-tablet has-text-left subtitle">12:30</div>
+        <div class="column is-7">
+          <div class="content is-large">
+            <pre><code class="hljs javascript">{{{jsCode | highlight 'javascript'}}}</code></pre>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="hero is-success">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1">
+          Full Height title
+        </h1>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile has-text-right subtitle">12:30</div>
+        <div class="column is-3 is-hidden-tablet has-text-left subtitle">12:30</div>
+        <div class="column is-7">
+          <div class="content is-large">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/g_fDdnIsrNU" frameborder="0" allowfullscreen></iframe>
+          </div>
+          <article class="media">
+            <figure class="media-left">
+              <p class="image is-64x64">
+                <img src="http://placehold.it/128x128">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Barbara Middleton</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
+                </p>
+              </div>
+            </div>
+          </article>
+          <article class="media">
+            <figure class="media-left">
+              <p class="image is-64x64">
+                <img src="http://placehold.it/128x128">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Barbara Middleton</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
+                </p>
+              </div>
+            </div>
+          </article>
+          <article class="media">
+            <figure class="media-left">
+              <p class="image is-64x64">
+                <img src="http://placehold.it/128x128">
+              </p>
+            </figure>
+            <div class="media-content">
+              <p class="control">
+                <textarea class="textarea" placeholder="Add a comment..."></textarea>
+              </p>
+              <p class="control">
+                <button class="button">Post comment</button>
+              </p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile has-text-right subtitle">12:30</div>
+        <div class="column is-3 is-hidden-tablet has-text-left subtitle">12:30</div>
+        <div class="column is-7">
+          <div class="content is-large">
+            <iframe width="100%" height="300" src="//jsfiddle.net/gnkbe2Lq/embedded/result,html,css,js/light/" allowfullscreen="allowfullscreen" frameborder="0"></iframe>
+          </div>
+          <article class="media">
+            <figure class="media-left">
+              <p class="image is-64x64">
+                <img src="http://placehold.it/128x128">
+              </p>
+            </figure>
+            <div class="media-content">
+              <p class="control">
+                <textarea class="textarea" placeholder="Add a comment..."></textarea>
+              </p>
+              <p class="control">
+                <button class="button">Post comment</button>
+              </p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile has-text-right subtitle">12:30</div>
+        <div class="column is-3 is-hidden-tablet has-text-left subtitle">12:30</div>
+        <div class="column is-7">
+          <div class="content is-large">
+            <pre><code class="hljs javascript">{{{jsCode | highlight 'javascript'}}}</code></pre>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile has-text-right subtitle">12:30</div>
+        <div class="column is-3 is-hidden-tablet has-text-left subtitle">12:30</div>
+        <div class="column is-7">
+          <div class="content is-large">
+            <pre><code class="hljs css">{{{cssCode | highlight 'css'}}}</code></pre>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile has-text-right subtitle">12:30</div>
+        <div class="column is-3 is-hidden-tablet has-text-left subtitle">12:30</div>
+        <div class="column is-7">
+          <div class="content is-large">
+            <pre><code class="hljs html">{{{htmlCode | highlight 'html'}}}</code></pre>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3 is-hidden-mobile has-text-right subtitle">12:30</div>
+        <div class="column is-3 is-hidden-tablet has-text-left subtitle">12:30</div>
+        <div class="column is-7">
+          <div class="content is-large text">
+            <h1>Hello World</h1>
+            <!-- <pre><code class="hljs javascript">{{{jsCode | highlight 'javascript'}}}</code></pre> -->
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+            <h2>Second level</h2>
+            <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
+            <ul>
+              <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+              <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+              <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
+              <li>Ut non enim metus.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+// import Hello from './components/Hello'
+// import Prism from 'prismjs'
+import hljs from 'highlight.js'
+
+export default {
+  data () {
+    return {
+      jsCode: 'function foo () {\n  let x = 345;\n  return x;\n}',
+      cssCode: 'body: {\n  color: #FF005D;\n}',
+      htmlCode: '<div class="btn">\n  <h1>Hello</h1>\n</div>'
+    }
+  },
+  components: {},
+  filters: {
+    reverse: function (value) {
+      return value.split('').reverse().join('')
+    },
+    highlight: function (value, lang) {
+      return hljs.highlightAuto(value, [lang]).value
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bulma";
+@import "../node_modules/highlight.js/styles/tomorrow-night";
+.text {
+  background: #F3F5F6;
+  padding: 20px;
+}
+
+// .is-64x64 {
+//   border-radius: 26px;
+//   overflow: hidden;
+// }
+// .text {
+//   border: 1px solid #fce473;
+//   border-top-right-radius: 3px;
+//   color: rgba(17, 17, 17, 0.5);
+//   position: relative;
+// }
+// .text:before {
+//   background: #fce473;
+//   border-radius: 3px 3px 0 0;
+//   bottom: 100%;
+//   content: "Example";
+//   display: inline-block;
+//   font-size: 7px;
+//   font-weight: bold;
+//   left: -1px;
+//   letter-spacing: 1px;
+//   padding: 3px 5px;
+//   position: absolute;
+//   text-transform: uppercase;
+//   vertical-align: top;
+// }
+</style>
