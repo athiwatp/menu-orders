4f2ad2e6258f9be32a03c35162106220bf77ec84
Nati Namvong <nati@flyingcomma.com>
2016-07-12 19:19:53 +0700

    add item to order

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@
   <section class="section" v-if="user">
     <div class="container">
       <div class="columns">
-        <div class="column">
+        <div class="column is-12">
           <img :src="userPhotoURL" alt="" />
           <button class="button is-large" @click="logout">Logout</button>
-          <hr>
-          <span class="tag is-warning is-large" v-for="item in items">
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-6">
+          <span class="tag is-warning is-large" v-for="item in items" @click="addItemToOrder(item)">
             {{ item.text }}
             <button class="delete" @click="removeTodo(item['.key'])"></button>
           </span>
@@ -26,15 +29,23 @@
           <button class="button is-danger is-outlined is-large" @click="clearTodo">Clear</button>
           <button class="button is-large" @click="log">log</button>
         </div>
+
+        <div class="column is-6">
+          <span class="tag is-warning is-large" v-for="item in order">
+            {{ item.text }}
+            <img class="order-item-owner" :src="item.user.photoURL" alt="" width="24px" height="24px"/>
+            <button class="delete" v-if="item.user.uid === user.uid" @click="removeItemFromOrder(item['.key'])"></button>
+          </span>
+        </div>
       </div>
     </div>
   </section>
 
-  <hr>
+  <!-- <hr>
 
   <pre>
     {{user | json}}
-  </pre>
+  </pre> -->
 
 </template>
 
@@ -50,6 +61,8 @@ var config = {
 }
 firebase.initializeApp(config)
 var itemsRef = firebase.database().ref('items')
+var orderRef = firebase.database().ref('order')
+
 var provider = new firebase.auth.FacebookAuthProvider()
 
 export default {
@@ -69,7 +82,8 @@ export default {
   },
   firebase: {
     // can bind to either a direct Firebase reference or a query
-    items: itemsRef.orderByChild('text')
+    items: itemsRef.orderByChild('text'),
+    order: orderRef
   },
   created: function () {
     let vm = this
@@ -105,6 +119,21 @@ export default {
         // reset input box
         this.newTodo = ''
       }
+    },
+    addItemToOrder: function (item) {
+      let { uid, displayName, photoURL } = this.user
+      let newItem = {
+        text: item.text,
+        user: {
+          uid,
+          displayName,
+          photoURL
+        }
+      }
+      orderRef.push(newItem)
+    },
+    removeItemFromOrder: function (key) {
+      orderRef.child(key).remove()
     }
   }
 }
@@ -114,6 +143,10 @@ export default {
 @import "~bulma";
 
 .tag {
-  margin-right: 20px;
+  margin: 10px;
+}
+.order-item-owner {
+  border-radius: 12px;
+  margin: auto 2px auto 6px;
 }
 </style>
