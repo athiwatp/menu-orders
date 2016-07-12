6d11bb779ad1b4c439bbe715aba812d6c2b68a41
Supakorn <nuuknuuk14@gmail.com>
2016-07-12 16:06:24 +0700

    show order after click button add from menu

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
   <section class="section">
     <div class="container">
       <div class="columns">
-        <div class="column">
-          <span class="tag is-warning is-large" v-for="item in items">
+        <div class="menu-ordersBox">
+          <span class="" v-for="item in items">
             {{ item.text }}
             <button class="delete" @click="removeTodo(item['.key'])"></button>
+            <button @click="addToOrder(item)">Add </button><br>
           </span>
           <br><br>
-          <form @submit.prevent="addTodo">
+          <form @submit.prevent="addToMenu">
             <p class="control has-addons">
               <input class="input is-primary is-large" v-model="newTodo">
-              <a class="button is-info is-large">
+              <a class="button is-info is-large" @click="addToMenu">
                 Add #{{ items.length }}
               </a>
             </p>
@@ -20,6 +21,10 @@
           <hr>
           <button class="button is-danger is-outlined is-large" @click="clearTodo">Clear</button>
           <button class="button is-large" @click="log">log</button>
+          <hr>
+          <div class="" v-for="menuItem in order">
+            {{menuItem.text}}
+          </div>
         </div>
       </div>
     </div>
@@ -37,6 +42,7 @@ var config = {
 }
 firebase.initializeApp(config)
 var itemsRef = firebase.database().ref('items')
+var orderRef = firebase.database().ref('order')
 
 export default {
   data () {
@@ -47,9 +53,16 @@ export default {
   components: {},
   firebase: {
     // can bind to either a direct Firebase reference or a query
-    items: itemsRef.orderByChild('text')
+    items: itemsRef.orderByChild('text'),
+    order: orderRef.orderByChild('text')
   },
   methods: {
+    addToOrder: function (item) {
+      let newItem = {
+        text: item.text
+      }
+      orderRef.push(newItem)
+    },
     log: function () {
       console.log(this.items)
     },
@@ -59,7 +72,7 @@ export default {
     clearTodo: function () {
       itemsRef.set([])
     },
-    addTodo: function () {
+    addToMenu: function () {
       if (this.newTodo.trim()) {
         // update the Firebase reference!
         itemsRef.push({
@@ -75,8 +88,12 @@ export default {
 
 <style lang="scss">
 @import "~bulma";
-
+.menu-ordersBox {
+  width: 300px;
+  height: 700px;
+  border:1px dotted #CCCCCC;
+}
 .tag {
-  margin-right: 20px;
+  margin-right: 100vh;
 }
 </style>
